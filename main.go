1068a4089edb5bdc1f9340b7cfc1c7ce5ab03cf0
Mateusz Kwiatkowski <mateusz.kwiatkowski@conciso.de>
2022-01-10 11:10:07 +0100

    --amend

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	logger := log.New(os.Stdout, "go-microservice", log.LstdFlags)
+	logger := log.New(os.Stdout, "[go-microservice] *** ", log.LstdFlags)
 
 	th := handlers.NewTeams(logger)
 
